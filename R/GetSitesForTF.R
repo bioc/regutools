1a9531f3a6e4a565437938beab26c8ae8e47f273
Joselyn Chavez <joselynchavezf@gmail.com>
2019-06-10 14:16:52 -0500

    Params in ‘GetAttr’ fixed. Split in ‘tfbs.table’ fixed. ‘evidence’ option added.

--- R/GetSitesForTF.R
+++ R/GetSitesForTF.R
@@ -1,8 +1,9 @@
-#' @title Get binding sites for a Transcription Factor (TF)
+#' @title Get the binding sites for a Transcription Factor (TF)
 #' @description Retrieve the binding sites and genome location for a given transcription factor.
-#' @author José Alquicira Hernández & Jacques van Helden
-#' @param tf name of the transcription factor of interest
-#' @return a dataframe with the following columns:
+#' @author José Alquicira Hernández, Jacques van Helden, Joselyn Chávez
+#' @param TF name of the transcription factor of interest
+#' @return A dataframe with the following columns corresponding to the TFBSs associated with the TF.
+#' If the transcription factor does not exist, returns NA.
 #' \itemize{
 #' \item ID (identifier)
 #' \item left position
@@ -10,37 +11,47 @@
 #' \item strand
 #' \item sequence
 #' }
-#' corresponding to the TFBSs associated with the TF.
-#' If the transcription factor does not exist, returns a NA.
 #' @examples
-#' ## Extract the binding sites for AraC
-#' tfbs <- GetSitesForTF(tf = "AraC")
-#' @export
+#' ## Get the binding sites for AraC
 #'
+#' tfbs <- GetSitesForTF(TF = "AraC")
+#' @export
 
-GetSitesForTF <- function(tf) {
+GetSitesForTF <- function(TF, evidence = NULL) {
 
-  tfbs.raw <- tryCatch({
-    getAttr(attributes = c("name", "tfbs_unique"),
-            filters = "name",
-            values = tf,
-            dataset = "TF")
-  },error = function(cond) return(NULL))
+  if (!is.null(evidence)) {
+    tfbs.raw <- tryCatch({
+      GetAttr(dataset = "TF",
+              attributes = c("name", "tfbs_unique", "evidence_reference"),
+              filters = list(name = TF))
+    },error = function(cond) return(NULL))
+  } else {
+    tfbs.raw <- tryCatch({
+      GetAttr(dataset = "TF",
+              attributes = c("name", "tfbs_unique"),
+              filters = list(name = TF))
+    },error = function(cond) return(NULL))
+  }
+
+
 
   if(is.null(tfbs.raw)){
     return(NA)
   }else{
-    # convert raw info into a table with 1 row per TFBS
-    # and 1 col per attribute
-    tfbs.csv <- unlist(strsplit(x = tfbs.raw$tfbs_unique, split =  ";"))
-  tfbs.table <- as.data.frame(
-    Reduce(rbind, strsplit(x = tfbs.csv, split = ",")), row.names = FALSE)
-
-  ## TO DO:
-  ## - add chromosome (for future multi-organisms RegulonDB)
-  ## - add evidence
-  names(tfbs.table) <- c("ID", "left", "right", "strand", "sequence")
-
-  return(tfbs.table)
+    # convert raw info into a table with 1 row per TFBS and 1 col per attribute
+    tfbs.table <-  as.data.frame(strsplit(x = tfbs.raw$tfbs_unique, split = " "), col.names = "V1")
+    tfbs.table <- t(as.data.frame(strsplit(x = as.character(tfbs.table$V1), split =  "\t")))
+    if (!is.null(evidence)) {
+      tfbs.table <- rbind(tfbs.table,as.data.frame(tfbs.raw$evidence_reference))
+      colnames(tfbs.table) <- c("ID", "left", "right", "strand", "sequence", "evidence_reference")
+    } else {
+      colnames(tfbs.table) <- c("ID", "left", "right", "strand", "sequence")
+    }
+    rownames(tfbs.table) <- NULL
+    return(as.data.frame(tfbs.table))
   }
 }
+
+## TO DO:
+## - add chromosome (for future multi-organisms RegulonDB)
+## - add evidence
