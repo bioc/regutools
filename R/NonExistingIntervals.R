447f427b040a400529f797dacea33b96d62f91a1
Joselyn Chavez <joselynchavezf@gmail.com>
2019-06-10 14:24:54 -0500

    Documentation updated

--- R/NonExistingIntervals.R
+++ R/NonExistingIntervals.R
@@ -1,22 +1,21 @@
-#' @title Constructs a particular logical condition to query database
-#' @description Given a list of filters, this function builds a logical condition to query database using intervals
-#' @author
-#' Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández
-#' @param filters A list with values to restrict the query. The names of all elements in this list correspond to the attributes to bre retrieved.
-#' @param interv the filters that are going to be in a intervale
-#' @param operator A string indicading if all conditions -\code{filters}- (AND) or some of them (OR) should be met
+#' @title Constructs a logical condition to query database
+#' @description Given a list of filters, this function builds a logical condition to query database using intervals.
+#' The output is used in \code{\link{BuildCondition}}.
+#' @author Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández
+#' @param filters List of filters to be used. The names should correspond to the attribute and the values correspond to the condition for selection.
+#' @param interval the filters whose values will be considered as interval
+#' @param operator A string indicating if all the filters (AND) or some of them (OR) should be met
 #' @return A string with a sql logical condition to query the dataset
 #' @examples
-#' BuildCondition(filters = list(network_type = "TF-GENE",
-#'                          regulator_name = "AraC"),
-#'                           dataset = "NETWORK",
-#'                           operator = "AND")
+#' NonExistingIntervals(filters = list(name="ara",strand = "for"),
+#'                      interval = NULL,
+#'                      operator = "AND",
+#'                      partialmatch = c("name", "strand"))
 #' @export
 
-
-NonExistingIntervals<-function(filters, interv, operator, partialmatch){
-  if (!(length(partialmatch)+length(interv)==length(filters))){
-  non.interv.index<-!((names(filters) %in% interv)|(names(filters) %in% partialmatch))
+NonExistingIntervals<-function(filters, interval, operator, partialmatch){
+  if (!(length(partialmatch)+length(interval)==length(filters))){
+  non.interv.index<-!((names(filters) %in% interval)|(names(filters) %in% partialmatch))
   non.interv <-filters[non.interv.index]
   condition.format.non.interv <- mapply(paste0, filters[non.interv.index], "'", SIMPLIFY = FALSE)
   condition.format.non.interv <- mapply(paste, names(condition.format.non.interv), condition.format.non.interv, sep = " = '" , SIMPLIFY = FALSE)
@@ -26,7 +25,7 @@ NonExistingIntervals<-function(filters, interv, operator, partialmatch){
   }
   if (!is.null(partialmatch)){
     condition.partialmatch<-ExistingPartialMatch(filters, partialmatch, operator)
-    if ((length(partialmatch)+length(interv)==length(filters))){
+    if ((length(partialmatch)+length(interval)==length(filters))){
       return(condition.partialmatch)
     }else{
     condition.partialmatch<-ExistingPartialMatch(filters, partialmatch, operator)
