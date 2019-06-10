cf976c1f1586a9979188510b4ed217862b0d5174
Joselyn Chavez <joselynchavezf@gmail.com>
2019-06-10 14:11:26 -0500

    Output fixed: split in seq.string modified. Description and Params updated.

--- R/GetSiteSequences.R
+++ R/GetSiteSequences.R
@@ -1,13 +1,13 @@
 #' @title Extract sequences from a set of Transcription Factor Binding Sites (TFBSs)
-#' @description Returns the sequences for a set of binding sites.
-#' @author José Alquicira Hernández & Jacques van Helden
-#' @param tfbs A collection of sites previously retrieved from RegulonDB with the function \code{GetSitesForTF()}
+#' @description Returns the sequences for a set of Transcription Factor Binding Sites.
+#' @author José Alquicira Hernández, Jacques van Helden, Joselyn Chávez.
+#' @param tfbs A collection of sites previously retrieved from RegulonDB with the function \code{\link{GetSitesForTF}}
 #' @param seq.format Default: \code{fasta} output format. Supported: \code{fasta}, \code{wconsensus}.
-#' @return a string with the sequences
+#' @return A string with the TFBS sequences with "\n" breakline character.
 #' @examples
 #' ## Get the binding sites for AraC and extract their sequence in fasta format
-#' tfbs <- GetSitesForTF(tf = "AraC")
-#' tfbs.seq <- GetSiteSequences(tfbs) # This returns a single string with "\n" breakline character
+#' tfbs <- GetSitesForTF(TF = "AraC")
+#' tfbs.seq <- GetSiteSequences(tfbs)
 #' cat(tfbs.seq)
 #' @export
 
@@ -15,14 +15,13 @@
 GetSiteSequences <- function(tfbs, seq.format = "fasta") {
 
   if(!(is.data.frame(tfbs) & all(names(tfbs) %in% c("ID", "left", "right", "strand", "sequence")))){
-    stop("Must provide a valid colletion of sites retrieved with GetSitesForTF() function")
+    stop("Must provide a valid collection of sites retrieved with 'GetSitesForTF' function")
   }
 
   if (seq.format == "fasta") {
 
     header <- paste0(">", tfbs$ID, "_", tfbs$left, ":", tfbs$right, ":", tfbs$strand)
-    seq.string <- paste(header, as.vector(tfbs$sequence), collapse = "\n", sep = "\n")
-
+    seq.string <- paste0(header,"\n",as.vector(tfbs$sequence))
   } else {
 
     stop(paste(seq.format, " is not a valid format for GetSiteSequences()."))
