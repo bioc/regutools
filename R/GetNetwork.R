21c711aa7d2acdb3496a406ca7e72a89ad85a659
Joselyn Chavez <joselynchavezf@gmail.com>
2019-06-11 12:18:23 -0500

    regulator filter added

--- R/GetNetwork.R
+++ R/GetNetwork.R
@@ -1,25 +1,38 @@
 #' @title Return complete regulatory network.
 #' @description This function retrieves all the regulation networks in regulonDB between TF-TF, GENE-GENE or TF-GENE depending on the parameter 'type'
+#' @param regulator Name of TF or gene that acts as regulator. If NULL, the function retrieves all existent networks in the regulonDB.
 #' @param type "TF-GENE", "TF-TF", "GENE-GENE"
 #' @keywords regulation retrieval, TF, networks,
-#' @author Carmina Barberena Jonas, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández.
+#' @author Carmina Barberena Jonas, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández, Joselyn Chávez
 #' @examples
 #' # Retrieve regulation of 'araC'
 #'
-#' GetNetwork(type="TF-GENE")
+#' GetNetwork(regulator = "AraC", "type="TF-GENE")
+#'
+#' # Retrieve all GENE-GENE networks
+#'
+#' GetNetwork(type = "GENE-GENE")
 #' @export
 
-GetNetwork<-function(type="TF-GENE"){
+GetNetwork<-function(regulator = NULL, type="TF-GENE"){
   #Check type parameter
   if(! type %in% c("GENE-GENE","TF-GENE","TF-TF")){
     stop("Parameter 'type' must be TF-GENE, TF-TF, or GENE-GENE.",call.=FALSE)
   }
 
   #Get Network
-  network<-GetAttr(attributes=c("regulator_name","regulated_name","effect"),
-                   filters=list("network_type"=c(type)),
-                   dataset="NETWORK")
-  colnames(network)<-c("regulator","gene","effect")
+  if (!is.null(regulator)) {
+    network<-GetAttr(attributes=c("regulator_name","regulated_name","effect"),
+                                             filters=list(
+                                               "network_type"= type,
+                                               "regulator_name" = regulator),
+                                             dataset="NETWORK")
+  } else {
+    network<-GetAttr(attributes=c("regulator_name","regulated_name","effect"),
+                     filters=list("network_type"=c(type)),
+                     dataset="NETWORK")}
+
+  colnames(network)<-c("regulator","regulated","effect")
 
   #Change effect to +, - and +/-
   network$effect<-sub(pattern="activator",replacement="+",x=network$effect)
