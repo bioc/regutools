c3ff6458a1290273cb6edbff1b96703d6ed204a5
Joselyn Chavez <joselynchavezf@gmail.com>
2019-06-17 13:41:56 -0500

    Examples added

--- R/RetrieveSeq.R
+++ R/RetrieveSeq.R
@@ -4,26 +4,35 @@
 #' @param organism Organism. Words need to be underscore separated (example: Escherichia_coli_K12).
 #' @param genes A list of genes
 #' @param all Return sequences for all the genes of the organism if TRUE. Incompatible with `genes`.
-#' @param noorf Prevent overlap with upstream open reading frames (ORF) if value TRUE.
+#' @param noorf Prevents overlap with upstream open reading frames (ORF) if TRUE.
 #' @param from Inferior limit of the region to retrieve. Default is organism dependant (example: Saccharomyces cerevisiae = -800).
 #' @param to Superior limit of the region to retrieve. Default is '-1'.
 #' @param feattype Type of genome features to load. Supported: CDS, mRNA, tRNA, rRNA.
 #' @param type Sequence type. Supported: 'upstream', 'downstream', 'ORF' (unspliced open reading frame).
-#' @param format Sequence format. Supported: IG (Intelligenetics), WC (wconsensus), raw, fasta
+#' @param format Sequence format. Supported: 'IG' (Intelligenetics), 'WC' (wconsensus), 'raw', 'fasta'
 #' @param lw Line width (0 for whole sequence on one line).
-#' @param label Field(s) to be used in the sequence label. Multiple fields can be specified, separated by commas. Supported: id, name, organism_name, sequence_type, current_from, current_to, ctg, orf_strand, reg_left, reg_right. Default: name.
+#' @param label Field(s) to be used in the sequence label. Multiple fields can be specified, separated by commas. Supported: 'id', 'name', 'organism_name', 'sequence_type', 'current_from', 'current_to', 'ctg', 'orf_strand', 'reg_left', 'reg_right'. Default: name.
 #' @param label.sep Separator between the label fields. Default: '|' (pipe character).
-#' @param nocom No comments if TRUE. Only the identifier and the sequence are returned. By default, the comment indicates the ORF and upstream sequence coordinates.
+#' @param nocom No comments are shown if TRUE, only the identifier and the sequence are returned. By default, the comment indicates the ORF and upstream sequence coordinates.
 #' @param repeat.mask Use the repeat masked version of the genome if TRUE. Attention: repeated regions are annotated for some genomes only.
-#' @param imp.pos Admit imprecise positions if value TRUE
-#' @return an string with results retrieved from RSAT
+#' @param imp.pos Admit imprecise positions if TRUE.
+#' @return An string with results retrieved from RSAT
 #' @examples
-#' # Retrieve sequence for araC gene in *E.coli* from -70 to +70.
-#' res <- RetrieveSeq(organism = "Escherichia_coli_K12", genes = 'araC')
+#' # Retrieve sequence for araC gene in E.coli from -70 to +70.
+#' res <- RetrieveSeq(organism = "Escherichia_coli_K12",
+#'                    genes = "araC",
+#'                    from = -70,
+#'                    to = 70,
+#'                    format = "raw")
+#'
+#' # Retrieve sequences for rpoS and rpoE CDS without comments.
+#' res <- RetrieveSeq(organism = "Escherichia_coli_K12",
+#'                    genes = c("rpoS", "rpoE"),
+#'                    feattype = "CDS",
+#'                    format = "fasta",
+#'                    nocom = TRUE)
 #' cat(res)
 #' @export
-
-
 
 RetrieveSeq <- function(organism, genes = NULL, all = FALSE, noorf = FALSE, from = NULL , to = NULL,
                          feattype = NULL, type = NULL, format = NULL, lw = NULL, label = NULL,
