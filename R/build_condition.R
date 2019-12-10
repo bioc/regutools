4dd1408911cb5cf1ac60ab68bba188be0443f808
Joselyn Chavez <joselynchavezf@gmail.com>
2019-12-10 17:29:52 -0600

    regulondb param added in examples and documentation

--- R/build_condition.R
+++ R/build_condition.R
@@ -2,14 +2,17 @@
 #' @description Given a list of filters, this function builds a logical condition to query database.
 #' The output is used in \code{\link{GetAttr}}.
 #' @author Carmina Barberena Jonás, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández, Joselyn Chávez
+#'
 #' @param dataset dataset of interest
 #' @param filters List of filters to be used. The names should correspond to the attribute and the values correspond to the condition for selection.
 #' @param operator A string indicating if all the filters (AND) or some of them (OR) should be met
 #' @param interval the filters with values considered as interval
+#' @param regulondb A regulondb object.
 #' @param partialmatch name of the condition(s) with a string pattern for full or partial match in the query
+#'
 #' @return A string with a sql logical condition to query the dataset
 #' @examples
-#' buil_condition(dataset = "GENE",
+#' buil_condition(regulondb, dataset = "GENE",
 #'         filters = list(name=c("ara"),
 #'                        strand=c("forward"),
 #'                        posright=c("2000","40000")),
