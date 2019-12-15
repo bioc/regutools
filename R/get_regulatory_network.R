fec2a0e5785199c24fd738f09e833433a7ca96f5
Joselyn Chavez <joselynchavezf@gmail.com>
2019-12-15 16:50:09 -0600

    regulondb param added

--- R/get_regulatory_network.R
+++ R/get_regulatory_network.R
@@ -1,8 +1,10 @@
 #' @title Return complete regulatory network.
 #' @description This function retrieves all the regulation networks in regulonDB between TF-TF, GENE-GENE or TF-GENE depending on the parameter 'type'
+#' @param regulondb A regulondb object
 #' @param regulator Name of TF or gene that acts as regulator. If NULL, the function retrieves all existent networks in the regulonDB.
 #' @param type "TF-GENE", "TF-TF", "GENE-GENE"
 #' @param cytograph If TRUE, displays network in Cytoscape. This option requires previous instalation and launch of Cytoscape.
+#'
 #' @keywords regulation retrieval, TF, networks,
 #' @author Carmina Barberena Jonas, Jesús Emiliano Sotelo Fonseca, José Alquicira Hernández, Joselyn Chávez
 #' @examples
@@ -19,7 +21,7 @@
 #' get_regulatory_network(regulator = "AraC", type = "TF-GENE", cytograph = TRUE)
 #' @export
 
-get_regulatory_network<-function(regulator = NULL, type="TF-GENE", cytograph = FALSE){
+get_regulatory_network <- function(regulondb, regulator = NULL, type="TF-GENE", cytograph = FALSE){
   #Check type parameter
   if(! type %in% c("GENE-GENE","TF-GENE","TF-TF")){
     stop("Parameter 'type' must be TF-GENE, TF-TF, or GENE-GENE.",call.=FALSE)
@@ -27,15 +29,15 @@ get_regulatory_network<-function(regulator = NULL, type="TF-GENE", cytograph = F
 
   #Get Network
   if (!is.null(regulator)) {
-    network <- get_dataset(attributes=c("regulator_name","regulated_name","effect"),
+    network <- as.data.frame(get_dataset(regulondb, attributes=c("regulator_name","regulated_name","effect"),
                                              filters=list(
                                                "network_type"= type,
                                                "regulator_name" = regulator),
-                                             dataset="NETWORK")
+                                             dataset="NETWORK") )
   } else {
-    network <- get_dataset(attributes=c("regulator_name","regulated_name","effect"),
+    network <- as.data.frame(get_dataset(regulondb, attributes=c("regulator_name","regulated_name","effect"),
                      filters=list("network_type"=c(type)),
-                     dataset="NETWORK")}
+                     dataset="NETWORK") ) }
 
   if (cytograph) { #CytoScape conection
     tryCatch(cytoscapePing(), error = function(e) {stop("Launch Cytoscape before running GetNetwork()", call. = FALSE)})
