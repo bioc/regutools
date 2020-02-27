84be27cd288f663f61a231648ad557734d8ae283
Joselyn  Chavez <joselynchavezf@gmail.com>
2020-02-27 14:13:36 -0600

    delete test that expects GRanges from plot_dna_objects

--- tests/testthat/test-plot_dna_objects.R
+++ tests/testthat/test-plot_dna_objects.R
@@ -1,5 +1,5 @@
 context("plot_dna_objects")
-test_that("plot_dna_objects output is a GRanges object", {
+test_that("non-valid genomic elements causes error", {
     ## Connect to the RegulonDB database if necessary
     if (!exists('regulondb_conn'))
         regulondb_conn <- connect_database()
@@ -12,15 +12,6 @@ test_that("plot_dna_objects output is a GRanges object", {
             genome_version = "prueba",
             database_version = "prueba"
         )
-    reg_result <-
-        plot_dna_objects(regdb,
-            from = 5000,
-            to = 10000,)
-    expect_equivalent(class(reg_result), "GRanges")
-})
-
-
-test_that("non-valid genomic elements causes error ", {
     expect_error(plot_dna_objects(
         regdb,
         from = 5000,
