3850addba7f6b12806222b170c96f2099e0e403b
Joselyn  Chavez <joselynchavezf@gmail.com>
2020-02-28 21:38:04 -0600

    change parameters 'from' and 'to' to grange

--- R/plot_dna_objects.R
+++ R/plot_dna_objects.R
@@ -2,8 +2,7 @@
 #'
 #' @param regulondb A [regulondb()] object.
 #' @param genome A valid UCSC genome name.
-#' @param from A `integer(1)` specifying the left position.
-#' @param to A `integer(1)` specifying the right position.
+#' @param grange A [GenomicRanges::GRanges-class()] object indicating position left and right.
 #' @param elements A character vector specifying which annotation elements to
 #' plot. It can be any from: `"-10 promoter box"`, `"-35 promoter box"`,
 #' `"gene"`, `"promoter"`, `"Regulatory Interaction"`, `"sRNA interaction"`,
@@ -13,6 +12,8 @@
 #' @return A plot with genomic elements found within a genome region,
 #' including genes and regulators.
 #' @importFrom Gviz GeneRegionTrack plotTracks GenomeAxisTrack AnnotationTrack
+#' @importFrom GenomicRanges GRanges
+#' @importFrom IRanges IRanges
 #' @examples
 #' ## Connect to the RegulonDB database if necessary
 #' if(!exists('regulondb_conn')) regulondb_conn <- connect_database()
@@ -30,21 +31,16 @@
 #' plot_dna_objects(e_coli_regulondb)
 #'
 #' ## Plot genes providing Genomic Ranges
-#' plot_dna_objects(e_coli_regulondb, from = 5000, to = 10000)
+#' plot_dna_objects(e_coli_regulondb, grange = GenomicRanges::GRanges("chr", IRanges::IRanges(5000, 10000)) )
 #'
 #' ## Plot aditional elements within genomic positions
-#' plot_dna_objects(e_coli_regulondb, from = 5000, to = 10000, elements = c("gene", "promoter"))
+#' plot_dna_objects(e_coli_regulondb, grange = GenomicRanges::GRanges("chr", IRanges::IRanges(5000, 10000)), elements = c("gene", "promoter"))
 #' @export
 plot_dna_objects <-
     function(regulondb,
         genome = "eschColi_K12",
-        from = 0,
-        to = 5000,
+        grange = GRanges("chr", IRanges(1, 5000) ),
         elements = "gene") {
-        # validate ranges
-        if (!is.numeric(from) || !is.numeric(to)) {
-            stop("Parameter 'from' and 'to' must be a number", call. = FALSE)
-        }
 
         valid_elements <- c(
             "-10 promoter box",
@@ -71,7 +67,7 @@ plot_dna_objects <-
         dna_objects <- regutools::get_dataset(
             regulondb,
             dataset = "DNA_OBJECTS",
-            filters = list(posright = c(from, to),
+            filters = list(posright = c(grange@ranges@start, grange@ranges@start + grange@ranges@width),
                 type = elements),
             interval = "posright",
             output_format = "GRanges"
